a76e5175904023295131ba813af4ea314f3a4987
Paulo M. Leite <paulo.m.leite@globaleda.pt>
2022-03-18 13:17:53 -0100

    Commit made with bulk tool!

--- modules/git_operations.sh
+++ modules/git_operations.sh
@@ -4,30 +4,25 @@ git-operation()
 {
   case $1 in
     "--status")
+      echo $1 $2 $3 $4 $5
       message 1 $2
       git -C $2 status
     ;;
     "--pull")
+      echo $1 $2 $3 $4 $5
       message 2 $2 $5
       git -C $2 checkout $5
       git -C $2 pull
     ;;
     "--push")
+      echo $1 $2 $3 $4 $5
       message 3 $2 $5
       git -C $2 add -A 
       git -C $2 commit -m "$DEFAULT_COMMIT_MSG"
       git -C $2 push
     ;;
-    "checkfolder")
-      if [ $3 = "0" ]
-        then
-         git-operation $4 $5 $6
-        else
-         message 4 $5
-      fi
-    ;;
     "--bulk")
-      echo $1 $2 $3 $4 $5 $6
+      echo $1 $2 $3 $4 $5
       for d in */
         do
           echo $d
