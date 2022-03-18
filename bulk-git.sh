defcb757ec860cc0a7e904eaa31171f41452c6c5
Paulo M. Leite <paulo.m.leite@globaleda.pt>
2022-03-18 15:05:39 -0100

    Commit made with bulk tool!

--- bulk-git.sh
+++ bulk-git.sh
@@ -3,9 +3,162 @@
 # By.:Paulo Leite
 # Ver: 1.5
 
-source ./modules/vars.sh
-source ./modules/help_and_msgs.sh
-source ./modules/git_operations.sh
+
+# Default Values
+DEFAULT_COMMIT_MSG="Commit made with bulk tool!"
+
+# Color Values
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+OFF='\033[0m'
+
+# Messages
+STATUS_MSG="Status of the repository"
+BRANCH_MSG="of branch"
+PULL_MSG="Pulling repository"
+PUSH_MSG="Pushing repository"
+NOT_REPOSITORY_MSG="this is not a GIT repository"
+WRONG_REPOSITORY_MSG="One or more parameters is/are not a GIT repository folders! For instructions Type: ./bulk-git.sh --help"
+INVALID_BULK_OPERATION_MSG="Invalid bulk operation"
+INVALID_FOLDER_MSG="is not a GIT respository folder"
+INVALID_OPERATION_MSG="Invalid operation!"
+
+help()
+{
+    clear
+    echo
+    printf "bulk-git.sh is a ${RED}BASH${OFF} script that allows to do bulk GIT operations\n"
+    printf "in multiple repositories simultaneously.${OFF}\n"
+    echo
+    echo "How to use:"
+    echo
+    echo "bulk-pull [OPTIONAL] *[COMMAND] *[REPOSITORY]...[REPOSITORY] *[BRANCH]"
+    echo 
+    echo "OPTIONAL:"
+    echo "    --bulk                Scan all folder relative to the script location"   
+    echo "                          and does the bulk comand indicated."
+    echo
+    echo "                          How to use (status example):"   
+  printf "                          ${RED}./bulk-git.sh --bulk --status dev${OFF}\n"
+    echo                            
+    echo "                          Note: all * fields are demanded, even if not used"
+    echo "                          (will be fixed in the future)"
+    echo 
+    echo "COMMAND:"
+    echo "    --status              Return the git status of the repository(ies)"
+    echo "    --pull                Execute the git pull of the repository(ies)"
+    echo "    --push                Execute the git push of the repository(ies)"
+    echo "    --help                Show this help text" 
+    echo "                          How to use (example):"   
+  printf "                          ${RED}./bulk-git.sh --pull [COMMAND] [BRANCH]${OFF}\n"
+    echo         
+    echo "REPOSITORY:"
+    echo "    Repository Folder     Target folder, max 7"
+  printf "                          ${RED}must be a GIT folder${OFF}\n"
+    echo
+    echo "BRANCH:"
+    echo "    Git branch            Target branch, only used for pull but input is mandatory"
+    echo 
+    echo " *mandatory fields"
+    echo 
+}
+
+message()
+{
+  echo
+  case $1 in
+    1)
+      printf "${GREEN}$STATUS_MSG $2${OFF}:\n"
+    ;;
+    2)
+      printf "$PULL_MSG ${GREEN}[$2]${OFF} $BRANCH_MSG ${GREEN}[$3]:${OFF}\n"
+    ;;
+    3)
+      printf "$PUSH_MSG ${GREEN}[$2]${OFF} no branch atual${OFF}\n"
+    ;;
+    4)
+      printf "${RED}$NOT_REPOSITORY_MSG ${OFF}\n"
+    ;;
+    5)
+      printf "${RED}$WRONG_REPOSITORY_MSG ${OFF}\n"
+    ;;
+    6)
+      printf "${RED}$INVALID_BULK_OPERATION_MSG ${OFF}\n"
+    ;;
+    7)
+      printf "${RED}$2 $INVALID_FOLDER_MSG ${OFF}\n"
+    ;;
+    8)
+      printf "${RED}$INVALID_OPERATION_MSG ${OFF}\n"
+    ;;
+
+  esac
+  echo
+}
+
+git-operation()
+{
+  case $1 in
+    #Execute Status Operation
+    "--status")
+    if [ $3 = "0" ]
+      then
+        message 1 $2
+        git -C $2 status
+      else  
+        message 7 $2 
+    fi
+    ;;
+
+    #Execute Pull Operation
+    "--pull")
+    if [ $3 = "0" ]
+      then
+        message 2 $2 $5
+        git -C $2 checkout $5
+        git -C $2 pull
+      else
+        message 7 $2
+    fi
+    ;;
+
+    #Execute Push Operation
+    "--push")
+    if [ $3 = "0" ]
+      then
+        message 3 $2 $5
+        git -C $2 add -A 
+        git -C $2 commit -m "$DEFAULT_COMMIT_MSG"
+        git -C $2 push
+      else
+        message 7 $2
+    fi
+    ;;
+
+    #Execute Bulk Operations
+    "--bulk")
+      echo $1 $2 $3
+      if [ $2 = '--status' ] || [ $2 = '--pull' ] || [ $2 = '--push' ]
+        then
+          for d in */
+            do
+              CHECK="$(git -C $d/ rev-parse 2>/dev/null; echo $?)"
+              if [ $CHECK = "0" ]
+                then
+                  echo $d
+                  git-operation $2  $PWD/$d $CHECK ${@: -1}
+                else
+                  echo error!
+              fi
+            done
+        else
+        message 8 
+      fi
+    ;;
+  esac
+}
+
+
 
 if [ $# -lt 3 ] || [ $1 = '--help' ] || [ $# -gt 9 ] 
     then
